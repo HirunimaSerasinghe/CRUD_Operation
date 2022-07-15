e33bd88540c2b4006a95087f203d03d3f7eb0c63
HirunimaSerasinghe <88132673+HirunimaSerasinghe@users.noreply.github.com>
2022-07-16 00:34:58 +0530

    Added Home & AddNewRecord components

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import {BrowserRouter, Switch,Route} from "react-router-dom";
 import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Home from './Pages/Home'
+import AddNewRecord from './Pages/AddNewRecord'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <ToastContainer position='top-center' />
+        <Routes>
+          <Route exact path ="/" element={<Home/>}/>
+          <Route exact path ="/" element={<AddNewRecord/>}/>
+        </Routes>
       </div>
     </BrowserRouter>
   );
