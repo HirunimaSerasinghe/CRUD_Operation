5e530eccc449742e5d46ba2aa2fafe299108f23d
HirunimaSerasinghe <88132673+HirunimaSerasinghe@users.noreply.github.com>
2022-07-16 00:37:54 +0530

    Created home component with tables

--- Client/src/Pages/Home.js
+++ Client/src/Pages/Home.js
@@ -4,6 +4,7 @@ import "./Home.css";
 import {toast} from "react-toastify";
 import axios from "axios";
 
+
 const Home = () => {
     const [data,setData] = useState ([]);
     const loadData = async () => {
@@ -15,7 +16,46 @@ const Home = () => {
         loadData();
     }, []);
     return (
-        <div></div>
+        <div style={{marginTop:"150px"}}>
+            <Link to='/AddNewRecord'>
+                <button className="btn btn-content">Add a new record</button>
+            </Link>
+            <table className="styled-table">
+                <thead>
+                    <tr>
+                        <th style={{textAlign:"center"}}> Student ID</th>
+                        <th style={{textAlign:"center"}}> Student Name</th>
+                        <th style={{textAlign:"center"}}> Student Email</th>
+                        <th style={{textAlign:"center"}}> Guardian Name</th>
+                        <th style={{textAlign:"center"}}> Guardian Contact Number</th>
+                        <th style={{textAlign:"center"}}> Action</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item,index) => {
+                        return (
+                            <tr key={item.id}>
+                                <th scope="row">{index+1}</th>
+                                <td>{item.StudentName}</td>
+                                <td>{item.StudentEmail}</td>
+                                <td>{item.GuardianName}</td>
+                                <td>{item.GuardianContactNumber}</td>
+                                <td>
+                                    <Link to={'/update'}>
+                                        <button className="btn btn-edit">Edit</button>
+                                    </Link>
+                                    <button className="btn btn-delete">Delete</button>
+                                    <Link to={'/view'}>
+                                        <button className="btn btn-view">View</button>
+                                    </Link>
+                                </td>
+                            </tr>
+                        )
+                    } )}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
