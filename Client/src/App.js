f24c322acd388dc9124fb8ff9fe16237de16dc93
HirunimaSerasinghe <88132673+HirunimaSerasinghe@users.noreply.github.com>
2022-07-25 23:03:51 +0530

    Added update & view components

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import {BrowserRouter, Switch,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Home from './Pages/Home'
 import AddNewRecord from './Pages/AddNewRecord'
+import View from './Pages/View';
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
         <ToastContainer position='top-center' />
         <Routes>
           <Route exact path ="/" element={<Home/>}/>
-          <Route exact path ="/" element={<AddNewRecord/>}/>
+          <Route exact path ="/AddNewRecord" element={<AddNewRecord/>}/>
+          <Route exact path ="/update/:id" element={<AddNewRecord/>}/>
+          <Route exact path ="/View/:id" element={<View/>}/>
         </Routes>
       </div>
     </BrowserRouter>
